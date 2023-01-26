3446b1b8786a68a343ca2027d6ae2b99c3fa9213
L I L V C <simidawalang@gmail.com>
2023-01-26 21:31:20 +0100

    deployed contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config({ path: ".env" });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.QUICKNODE_HTTP_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,6 +12,7 @@ const deploy = async () => {
   const deployedContract = await orisaContract.deployed();
 
   console.log("The Orisa contract address: ", deployedContract.address);
+//   The Orisa contract address:  0xA7812F3460150653D15c76FF835b3Bb1Aa9D01Fd
 };
 
 deploy()
